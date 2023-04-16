55f5d9316e0e5be4ddc9fa59e0879c9190c19bba
Sadohpv <Sigaldphp@gmail.com>
2023-04-16 23:45:06 +0700

    layout-setup

---
+++ src/routes/index.js
@@ -0,0 +1,46 @@
+// Import Layout
+ import { HeaderOnly } from '~/components/Layout';
+
+
+// Import Pages
+import Home from '~/pages/Home/';
+import Follow from '~/pages/Follow/';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+
+//Public Route
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/follow',
+        component: Follow,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
+    }
+    
+];
+
+
+// Private Route
+const privateRoutes = [
+
+];
+
+export {publicRoutes, privateRoutes}
